afba323a9719e01c0e9022d0c9ab4b8200008c05
Evgeny <john-alp@mail.ru>
2022-07-27 11:31:01 +0300

    start js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use sctrict";
+
+// alert('Hello');
+
+// const result = confirm('May?');
+// console.log(result);
+
+// const answer = prompt("May?", "yes");
+// console.log(typeof(answer) );
+
+const answers = [];
+
+answers[0] = prompt('Ваше имя?', '');
+answers[1] = prompt('Ваша фамилия?', '');
+answers[2] = prompt('Ваш возвраст?', 18);
+
+ document.write(answers);
+
